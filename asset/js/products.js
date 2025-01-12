f1ea408277c2a8d9d59dbeb6a8edb7a27b16f3ce
monir <monir.bu18@gmail.com>
2025-01-12 21:28:50 +0600

    update

--- asset/js/products.js
+++ asset/js/products.js
@@ -1,31 +1,31 @@
 const products = [
     {
         id: 1,
-        name: 'Product 1',
-        description: 'Description for product 1.',
-        price: 19.99,
-        image: 'product1.jpg'
+        name: "Laptop",
+        description: "A laptop is a portable computer that's small enough to be used on your lap.",
+        price: 99.99,
+        image: "asset/img/laptop.jpg"
     },
     {
         id: 2,
-        name: 'Product 2',
-        description: 'Description for product 2.',
-        price: 29.99,
-        image: 'product2.jpg'
+        name: "Mobile",
+        description: "A wireless handheld device",
+        price: 50.99,
+        image: "asset/img/mobile.jpg"
     },
     {
         id: 3,
-        name: 'Product 3',
-        description: 'Description for product 3.',
-        price: 39.99,
-        image: 'product3.jpg'
+        name: "Bike",
+        description: "A bike is a pedal-powered two-wheeled vehicle.",
+        price: 239.99,
+        image: "asset/img/bike.jpg"
     },
     {
         id: 4,
-        name: 'Product 4',
-        description: 'Description for product 4.',
-        price: 49.99,
-        image: 'product4.jpg'
+        name: "Car",
+        description: "Toyota Bangladesh",
+        price: 649.99,
+        image: "asset/img/car.jpg"
     }
 ];
 
